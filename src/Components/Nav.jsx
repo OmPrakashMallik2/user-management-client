6c5e97dba1e933250b0091479bb82e42cefd7f61
Om Prakash Mallik <147716127+OmPrakashMallik2@users.noreply.github.com>
2024-04-25 14:05:33 +0530

    Update Nav.jsx

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -4,12 +4,13 @@ import { Link } from 'react-router-dom'
 function Nav() {
 
     return (
-        <div className='bg-white flex justify-center p-6'>
+        <div className='bg-white flex bg-blue-500 justify-center p-6'>
 
-            <Link to="/" className='text-center text-2xl font-bold text-blue-600'>User Management</Link>
+            <Link to="/" className='text-center text-2xl font-bold text-white'>User Management</Link>
 
         </div>
     )
 }
+
 
 export default Nav
